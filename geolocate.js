20601232e4309379f29392715029e8802285ba8b
Galen Broderick <galen.broderick@gmail.com>
2015-07-10 17:27:09 -0700

    first commit

---
+++ geolocate.js
@@ -0,0 +1,5 @@
+//SEATTLE
+// http://forecast.weather.gov/MapClick.php?lat=47.6097&lon=-122.3331&FcstType=json
+
+// geolocation example: https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation
+
