e9c25a78beb1ad2085b32179a796b25e5a17a2b2
Galen Broderick <galen.broderick@gmail.com>
2015-09-15 17:52:50 -0700

    add new dependencies and update gulpfile to use them

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
+
 var rubysass = require('gulp-ruby-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
+var autoprefixer = require('autoprefixer');
+var postcss = require('gulp-postcss');
+var cssnext = require('cssnext');
+
 var jade = require('gulp-jade');
+
 var jshint = require('gulp-jshint');
+
 var browsersync = require('browser-sync').create();
+
 
 gulp.task('serve', ['styles'], function() {
     browsersync.init({
@@ -34,13 +41,27 @@ gulp.task('styles', function() {
       console.error('Error', err.message);
     })
     .pipe(sourcemaps.write())
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
     .pipe(gulp.dest('./css/'))
+
     .pipe(browsersync.stream());
 });
+
+
+
+gulp.task('postprocessCSS', function() {
+  var processors = [
+    autoprefixer({browsers: ['last 2 versions'], cascade: false}),
+    cssnext()
+  ];
+
+    return gulp.src('./css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dest'))
+
+})
+
 
 // gulp.task('lint', function() {
 //   gulp.src('js/')
